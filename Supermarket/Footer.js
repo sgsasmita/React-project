d1b7f621b2baa04183e3030f0b59589ebfe7581f
sgsasmita <150886583+sgsasmita@users.noreply.github.com>
2024-12-17 21:32:49 +0530

    Create Footer.js

---
+++ Supermarket/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'; // Make sure to create this file for styling
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Supermarket Management System</p>
+    </footer>
+  );
+};
+
+export default Footer;
